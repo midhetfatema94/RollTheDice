6d28e872ced35ddd4b26f58431ea017b03d6b80f
Midhet Sulemani <midhetfatema94@gmail.com>
2021-01-18 20:12:46 +0300

    first commit

---
+++ RollTheDice/RollTheDiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  RollTheDiceApp.swift
+//  RollTheDice
+//
+//  Created by Waveline Media on 1/18/21.
+//
+
+import SwiftUI
+
+@main
+struct RollTheDiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
