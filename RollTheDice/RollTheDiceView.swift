af8ce0dd3b71bf572c6da7f2699e7c2e1de4d760
Midhet Sulemani <midhetfatema94@gmail.com>
2021-01-21 15:45:42 +0300

    added haptics

--- RollTheDice/RollTheDiceView.swift
+++ RollTheDice/RollTheDiceView.swift
@@ -6,6 +6,8 @@
 //
 
 import SwiftUI
+import CoreHaptics
+import GameplayKit
 
 struct RollTheDiceView: View {
     @State private var outcome = 0
@@ -14,7 +16,10 @@ struct RollTheDiceView: View {
     @State private var newDegree: Double = 180
     @State private var shouldRandomize = false
     @State private var shouldAnimate = false
+    
     @EnvironmentObject var results: DiceResults
+    
+    @State private var engine: CHHapticEngine?
     
     let randomizeTimer = Timer.publish(every: 0.1, on: .main, in: .common)
     let animationTimer = Timer.publish(every: 5, on: .main, in: .common)
@@ -22,14 +27,10 @@ struct RollTheDiceView: View {
     var body: some View {
         VStack(spacing: 50) {
             if showingTransition {
-                Image("dices")
-                    .resizable()
-                    .frame(width: 250, height: 250)
+                DiceImageView()
                     .transition(.pivot(oldDegree: self.oldDegree, newDegree: self.newDegree))
             } else {
-                Image("dices")
-                    .resizable()
-                    .frame(width: 250, height: 250)
+                DiceImageView()
             }
             
             Text("Result is: \(outcome)")
@@ -50,8 +51,8 @@ struct RollTheDiceView: View {
             
             guard self.shouldRandomize else { return }
             
-            let randomResult = Int.random(in: 1...results.faces)
-            outcome = randomResult
+            let dice = GKRandomDistribution(lowestValue: 1, highestValue: results.faces)
+            outcome = dice.nextInt()
         })
         .onReceive(animationTimer, perform: {_ in
             
@@ -60,22 +61,34 @@ struct RollTheDiceView: View {
             self.shouldRandomize = false
             self.completeRolling()
         })
+        .onAppear(perform: prepareHaptics)
     }
     
     func startRollDice() {
         shouldRandomize = true
         shouldAnimate = true
+        
         let _ = randomizeTimer.connect()
         let _ = animationTimer.connect()
+        
+        do {
+            try engine?.start()
+        } catch {
+            print("There was an error starting the engine: \(error.localizedDescription)")
+        }
+        
+        startRollDiceHaptic()
     }
     
     func completeRolling() {
-        let randomResult = Int.random(in: 1...results.faces)
+        //Gives a dice simulation from the GamePlayKit
+        let dice = GKRandomDistribution(lowestValue: 1, highestValue: results.faces)
         
         shouldAnimate = false
+        engine?.stop(completionHandler: nil)
         
         startRotation {
-            outcome = randomResult
+            outcome = dice.nextInt()
             
             let diceResult = Dice()
             diceResult.result = outcome
@@ -91,6 +104,40 @@ struct RollTheDiceView: View {
             newDegree += 180
         }
         completion?()
+    }
+    
+    func prepareHaptics() {
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
+
+        do {
+            self.engine = try CHHapticEngine()
+            try engine?.start()
+        } catch {
+            print("There was an error creating the engine: \(error.localizedDescription)")
+        }
+    }
+    
+    func startRollDiceHaptic() {
+        // make sure that the device supports haptics
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
+        var events = [CHHapticEvent]()
+
+        // create one intense, sharp tap
+        for i in stride(from: 0.1, to: 5, by: 0.1) {
+            let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: 1)
+            let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: 1)
+            let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: i)
+            events.append(event)
+        }
+
+        // convert those events into a pattern and play it immediately
+        do {
+            let pattern = try CHHapticPattern(events: events, parameters: [])
+            let player = try engine?.makePlayer(with: pattern)
+            try player?.start(atTime: 0)
+        } catch {
+            print("Failed to play pattern: \(error.localizedDescription).")
+        }
     }
 }
 
@@ -119,3 +166,10 @@ extension AnyTransition {
     }
 }
 
+struct DiceImageView: View {
+    var body: some View {
+        Image("dices")
+            .resizable()
+            .frame(width: 250, height: 250)
+    }
+}
